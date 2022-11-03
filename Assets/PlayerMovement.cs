5163ac58edda69e5e2e5d9e0dc017f60407777ee
Juice02 <tejasraghavendra.tr@gmail.com>
2022-11-03 20:49:38 +0530

    wack looking terrain
    tell me if this works otherwise we go back to old one suwiiiiiiiiiiiiii

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rg;
+    [SerializeField]float speed=5f;
+    [SerializeField]float jump=10f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask layer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rg=GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Horizontal=Input.GetAxis("Horizontal");
+        float Vertical=Input.GetAxis("Vertical");
+        rg.velocity=new Vector3(Horizontal*speed,rg.velocity.y,Vertical*speed);
+
+        if(Input.GetButtonDown("Jump") && GroundCheck()){
+            rg.velocity=new Vector3(0,jump,0);
+        }
+    }
+    bool GroundCheck(){
+        return Physics.CheckSphere(groundCheck.position,0.1f,layer);
+    }
+}
